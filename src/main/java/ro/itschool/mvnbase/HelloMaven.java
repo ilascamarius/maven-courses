d314afb1955b35ba43a4cd9af0143da10802f535
Cornel Marius Ilasca <ilasca_marius@yahoo.com>
2019-06-20 19:47:40 +0300

    use jfiglet

--- src/main/java/ro/itschool/mvnbase/HelloMaven.java
+++ src/main/java/ro/itschool/mvnbase/HelloMaven.java
@@ -1,13 +1,18 @@
 package ro.itschool.mvnbase;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
+import java.io.IOException;
+
 public class HelloMaven {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println(new HelloMaven().sayHello());
     }
 
-    public String sayHello() {
-        return "Hello Maven";
+    public String sayHello() throws IOException{
+        return FigletFont.convertOneLine("Cornel");
     }
+
 
 }
