69520c100a5842eea56efed183af6bf5f4ef4eb4
Cornel Marius Ilasca <ilasca_marius@yahoo.com>
2019-06-20 19:17:49 +0300

    mavne base project

---
+++ src/main/java/ro/itschool/mvnbase/HelloMaven.java
@@ -0,0 +1,13 @@
+package ro.itschool.mvnbase;
+
+public class HelloMaven {
+
+    public static void main(String[] args) {
+        System.out.println(new HelloMaven().sayHello());
+    }
+
+    public String sayHello() {
+        return "Hello Maven";
+    }
+
+}
